80d8745b7a63b89f25e9d50fd59c865e3bb56889
Eduarda Queiroz <143042084+MaduStar@users.noreply.github.com>
2024-09-11 08:28:10 -0300

    BigMonster.js

--- src/components/monster/BigMonster.js
+++ src/components/monster/BigMonster.js
@@ -1,7 +1,7 @@
-class BigMonster extends Character{
+class BigMonster extends Character {
     constructor() {
         super('Big Monster')
-        this.life = 80
+        this.life = 120
         this.attack = 15
         this.defense = 3
         this.maxLife = this.life
