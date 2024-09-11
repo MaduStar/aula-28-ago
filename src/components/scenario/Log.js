fbe1c4750dc9dfe537643ce7b018c7880ea0916e
Eduarda Queiroz <143042084+MaduStar@users.noreply.github.com>
2024-09-11 08:29:35 -0300

    Log.js

--- src/components/scenario/Log.js
+++ src/components/scenario/Log.js
@@ -1,16 +1,19 @@
-class Log{
-  list=[]
-  constructor(listElement) {
-      this.listElement = listElement
-  }
+class Log {
+    list = []
+    constructor(listElement) {
+        this.listElement = listElement
+    }
 
-  addMessage(msg) {
-      this.list.push(msg)
-      this.render()
-  }
+    addMessage(msg) {
+        this.list.push(msg)
+        this.render()
+    }
 
-  render() {
-      this.listElement.innerHTML = ''
-      for (let i in this.list) {
-        
+    render() {
+        this.listElement.innerHTML = ''
+        for (let i in this.list) {
+            this.listElement.innerHTML += `<li>${this.list[i]}</li>`
+        }
+    }
 }
+
